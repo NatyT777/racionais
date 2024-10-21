653c82f2edbf21d0bbc42c3f3db88b80c1761682
Naty <148580617+NatyT777@users.noreply.github.com>
2024-10-21 09:11:23 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,39 @@
+criaCartao(
+    'Quais os tipos de rap',
+    'Podendo ser grime, drill ou trap'
+)
+
+criaCartao(
+    'em que ano surgiu esse gênero',
+    'Surgiu na década de 1960, mas foi levado aos EUA em 1970 '
+)
+
+criaCartao(
+    'Em que lugar do mundo deu origem a esse gênero',
+    'Surgiu na Jamaica no ano de 1960'
+)
+
+criaCartao(
+    'Qual é a porcentagem de pessoas que ouvem esse gênero no Brasil',
+    'O Brasil é o terceiro paos que mas consome Hip Hop o gênero em 23% de participação'
+)
+
+criaCartao(
+    'Onde esse gênero é mais ouvido no Brasil',
+    '1°São Paulo, 2°Rio de Janeiro, 3° Belo Horizonte, 4° Campinas, 5° Porto Alegre, 6° Curitiba'
+)
+
+criaCartao(
+    'Quais foram os primeiros rappers a terem sucesso',
+    'Os primeiros a terem sucesso foram DJ Hum Thagde, depois deles vieram nomes como Racionais MCs e Detendos do Rap'
+)
+
+criaCartao(
+    'Em que ano o grupo ganhou rreconhecimento',
+    'Especificamente entre os anos 1992 e 1994'
+)
+
+criaCartao(
+    'Quais foram as primeiras músicas lançadas ',
+    'A primeira gravação do grupo foi feita em 1998, apareceram os dois primeiros sucessos do grupo: "Pânico na Zona Sul" e "Tempos Dificeis"'
+)
